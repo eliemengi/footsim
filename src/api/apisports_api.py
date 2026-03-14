ea36ca03afac02e3a045f9a35450479485cca6af
eliemengi <eliemengi2@gmail.com>
2026-03-14 04:35:29 +0100

    change

--- src/api/apisports_api.py
+++ src/api/apisports_api.py
@@ -3,7 +3,7 @@ import time
 import requests
 from dotenv import load_dotenv
 
-load_dotenv()
+load_dotenv() 
 
 API_KEY = os.getenv("FOOTBALL_API_KEY")
 BASE_URL = "https://api.football-data.org/v4"
